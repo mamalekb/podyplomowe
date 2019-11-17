b8e3daf6c688a0dcaa95d133bef1c6dd86747366
Malwina Małgorzata Małek <mamalekb@student.wszib.edu.pl>
2019-11-17 08:40:15 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            int a = 2;
+        System.out.println("----------------");
+            System.out.println("Dla a = " + a + ":");
+            System.out.println("Pole pierwszego kwadratu = " + (a * a) + " cm.");
+            System.out.println("Obwód tego kwadratu = " + (4*a) + " cm.");
+
+        System.out.println("----------------");
+            a=5;
+            System.out.println("Dla a = " + a + ":");
+            System.out.println("Pole drugiego kwadratu = " + (a * a) + " cm.");
+            System.out.println("Obwód tego kwadratu = " + (4*a) + " cm.");
+
+        System.out.println("----------------");
+            a=7;
+            System.out.println("Dla a = " + a + ":");
+            System.out.println("Pole trzeciego kwadratu = " + (a * a) + " cm.");
+            System.out.println("Obwód tego kwadratu = " + (4*a) + " cm.");
+
+        System.out.println("----------------");
+            a=9;
+            System.out.println("Dla a = " + a + ":");
+            System.out.println("Pole czwartego kwadratu = " + (a * a) + ".");
+            System.out.println("Obwód tego kwadratu = " + (4*a) + ".");
+
+
+
+    }
+}
